f4862c28f4fbed7e9e67b9d0d1e6cc53ea619112
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2020-12-16 16:28:34 +0300

    Added login handler for endpoints with dynamic paths.

--- ch/main.go
+++ ch/main.go
@@ -37,6 +37,7 @@ func main() {
 
 	http.Handle("/", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	// get the room going
