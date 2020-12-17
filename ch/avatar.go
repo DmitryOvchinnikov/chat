daef643b230a42767744f26a02739f718c629b31
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2020-12-17 22:06:49 +0300

    Fixing the existing implementations.

--- ch/avatar.go
+++ ch/avatar.go
@@ -3,7 +3,8 @@ package main
 import (
 	"errors"
 	"io/ioutil"
-	"path"
+	"path/filepath"
+	"strings"
 )
 
 // ErrNoAvatar is the error that is returned when the
@@ -25,23 +26,18 @@ type FileSystemAvatar struct{}
 var UseFileSystemAvatar FileSystemAvatar
 
 func (FileSystemAvatar) GetAvatarURL(u ChatUser) (string, error) {
-	if userID, ok := c.userData["userid"]; ok {
-		if userIDStr, ok := userID.(string); ok {
 
-			files, err := ioutil.ReadDir("avatars")
-			if err != nil {
-				return "", ErrNoAvatarURL
-			}
-
-			for _, file := range files {
-				if file.IsDir() {
-					continue
-				}
-				if match, _ := path.Match(userIDStr + "*", file.Name()); match {
-					return "/avatars/" + file.Name(), nil
-				}
-			}
-
+	files, err := ioutil.ReadDir("avatars")
+	if err != nil {
+		return "", ErrNoAvatarURL
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		fname := file.Name()
+		if u.UniqueID() == strings.TrimSuffix(fname, filepath.Ext(fname)) {
+			return "/avatars/" + fname, nil
 		}
 	}
 
@@ -53,13 +49,13 @@ type AuthAvatar struct{}
 var UseAuthAvatar AuthAvatar
 
 func (AuthAvatar) GetAvatarURL(u ChatUser) (string, error) {
-	if url, ok := c.userData["avatar_url"]; ok {
-		if urlStr, ok := url.(string); ok {
-			return urlStr, nil
-		}
+
+	url := u.AvatarURL()
+	if len(url) == 0 {
+		return "", ErrNoAvatarURL
 	}
 
-	return "", ErrNoAvatarURL
+	return u.AvatarURL(), nil
 }
 
 type GravatarAvatar struct{}
@@ -67,12 +63,7 @@ type GravatarAvatar struct{}
 var UseGravatar GravatarAvatar
 
 func (GravatarAvatar) GetAvatarURL(u ChatUser) (string, error) {
-	if userID, ok := c.userData["userid"]; ok {
-		if userIDStr, ok := userID.(string); ok {
-			return "//www.gravatar.com/avatar/" + userIDStr, nil
-		}
-	}
 
-	return "", ErrNoAvatarURL
+	return "//www.gravatar.com/avatar/" + u.UniqueID(), nil
 }
 
